dd99e4f52228b2e1c5da2eaf8862ac869376fc2b
befaci03 <yliansarra002@gmail.com>
2025-10-27 11:30:42 +0100

    wathever fuck it

--- src/ts/keyboard/keysym.ts
+++ src/ts/keyboard/keysym.ts
@@ -1,39 +1,32 @@
 import { keycodeKeysyms, keyidentifier_keysym, OSK_keyMappings } from "./keys.js";
-// TODO: make this shit more clear (maybe rewriting it?)
+// TODO: make this MORE clear because it's so hard (maybe rewriting?)
 
 export default function GetKeysym(keyCode: number, key: string, location: number): number | null {
-	let keysym = keysym_from_key_identifier(key, location) || keysym_from_keycode(keyCode, location);
-	return keysym;
+	return getKeysymFromIdentifier(key, location) ?? getKeysymFromKeycode(keyCode, location);
 }
 
-function keysym_from_key_identifier(identifier: string, location: number): number | null {
+function getKeysymFromIdentifier(identifier: string, location: number): number | null {
 	if (!identifier) return null;
 
-	let typedCharacter: string | undefined;
-
 	// If identifier is U+xxxx, decode Unicode character
-	const unicodePrefixLocation = identifier.indexOf('U+');
-	if (unicodePrefixLocation >= 0) {
-		const hex = identifier.substring(unicodePrefixLocation + 2);
-		typedCharacter = String.fromCharCode(parseInt(hex, 16));
-	} else if (identifier.length === 1) typedCharacter = identifier;
-	else return get_keysym(keyidentifier_keysym[identifier], location);
-
-	if (!typedCharacter) return null;
-
-	const codepoint = typedCharacter.charCodeAt(0);
-	return keysym_from_charcode(codepoint);
+	if (identifier.startsWith("U+")) {
+		const codepoint = parseInt(identifier.slice(2), 16);
+		return getKeysymFromCharcode(codepoint);
+	}
+	else if (identifier.length === 1) return getKeysymFromCharcode(identifier.charCodeAt(0)); // single character handling
+	else return getKeysymFromMapping(keyidentifier_keysym[identifier], location); // handle identifiers (like Shift Enter etc)
 }
 
-function get_keysym(keysyms: number[] | null, location: number): number | null {
+function getKeysymFromMapping(keysyms: number[] | null, location: number): number | null {
 	if (!keysyms) return null;
-	return keysyms[location] || keysyms[0];
+	return keysyms[location] ?? keysyms[0];
 }
 
-function keysym_from_charcode(codepoint: number): number | null {
+function getKeysymFromCharcode(codepoint: number): number | null {
+	//? some hex shit
 	if (isControlCharacter(codepoint)) return 0xff00 | codepoint;
-	if (codepoint >= 0x0000 && codepoint <= 0x00ff) return codepoint;
-	if (codepoint >= 0x0100 && codepoint <= 0x10ffff) return 0x01000000 | codepoint;
+	if (codepoint <= 0x00ff) return codepoint;
+	if (codepoint <= 0x10ffff) return 0x01000000 | codepoint;
 	return null;
 }
 
@@ -41,8 +34,8 @@ function isControlCharacter(codepoint: number): boolean {
 	return codepoint <= 0x1f || (codepoint >= 0x7f && codepoint <= 0x9f);
 }
 
-function keysym_from_keycode(keyCode: number, location: number): number | null {
-	return get_keysym(keycodeKeysyms[keyCode], location);
+function getKeysymFromKeycode(keyCode: number, location: number): number | null {
+	return getKeysymFromMapping(keycodeKeysyms[keyCode], location);
 }
 
 export function OSK_buttonToKeysym(button: string): number | null {
